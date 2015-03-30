3334b9cfc03d2b36349c44e902e9bd628b4d58cc
Daniel Zappala <daniel.zappala@gmail.com>
2015-03-29 21:45:20 -0600

    Shuffled midterm

--- views/winter2015.py
+++ views/winter2015.py
@@ -168,28 +168,29 @@ def winter2015schedule():
     s.week()
 
     d = s.day('March 31')
-    d.assignment('Midterm')
+    d.lecture('No Class')
 
     d = s.day('April 2')
-    d.reading('Chapter 7.1 - 7.2')
-    d.lecture('Multimedia Networks','Multimedia Applications and Streaming Video')
-    d.reading('Multimedia Applications and Streaming Video',static + 'multimedia-applications-and-streaming-video.pdf')
+    d.assignment('Midterm')
 
     s.week()
 
     d = s.day('April 7')
+    d.reading('Chapter 7.1 - 7.2')
+    d.lecture('Multimedia Networks','Multimedia Applications and Streaming Video')
+    d.reading('Multimedia Applications and Streaming Video',static + 'multimedia-applications-and-streaming-video.pdf')
+
+    d = s.day('April 9')
     d.lecture('Multimedia Networks','Voice-Over-IP')
     d.reading('Chapter 7.3')
     d.reading('Voice-Over-IP',static + 'voice-over-ip.pdf')
-
-    d = s.day('April 9')
-    d.lecture('Multimedia Networks','Network Support for Multimedia')
-    d.reading('Chapter 7.4 - 7.5')
     d.assignment('Homework: Wireless',term + 'homework/wireless')
 
     s.week()
 
     d = s.day('April 14')
+    d.lecture('Multimedia Networks','Network Support for Multimedia')
+    d.reading('Chapter 7.4 - 7.5')
     d.assignment('Homework: Multimedia Networks',term + 'homework/multimedia-networks')
     d.assignment('Lab: Routing',term + 'labs/routing')
 
